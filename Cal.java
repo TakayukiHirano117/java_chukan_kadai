123bd7c9c959a54c072c463b01107205df4fa928
平野 宇教 <82029019+TakayukiHirano117@users.noreply.github.com>
2023-02-16 18:19:58 +0900

    Update Cal.java

--- Cal.java
+++ Cal.java
@@ -5,22 +5,28 @@ import java.util.Deque;
 
 // 逆ポーランド記法の数式を計算する処理
 public class Cal {
-	public static int result(String rpnExpression) {
+	public static double result(String rpnExpression) {
+//		1+( 2 ( 3 + (( 5 / 2 ) - 3 )))
 		
 		String[] formulaArray = rpnExpression.split("");
-		Deque<Integer> stack = new ArrayDeque<Integer>();
+		Deque<Double> stack = new ArrayDeque<Double>();
 		
 		for(String e : formulaArray) {
 			if('0' <= e.charAt(0) && e.charAt(0) <= '9') {
-//				String型からintegerにキャスト
-				stack.push(Integer.parseInt(e));
+//				String型からDoubleにキャスト
+				stack.push(Double.parseDouble(e));
 			} else {
-				int a = stack.pop();
-				int b = stack.pop();
+				double a = stack.pop();
+				double b = stack.pop();
 				if(e.equals("*")) {
 					stack.push(b * a);
 				} else if(e.equals("/")) {
-					stack.push(b / a);
+					try {
+						stack.push(b / a);
+					} catch(ArithmeticException error) {
+						System.out.println("エラー：0で割ることはできません。");
+					}
+					
 				} else if(e.equals("+")) {
 					stack.push(b + a);
 				} else if(e.equals("-")) {
@@ -29,7 +35,7 @@ public class Cal {
 			}
 		}
 		
-		int result = stack.pop();
+		double result = stack.pop();
 		return result;
 	}
 }
