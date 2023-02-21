6a8bcf0466c929a972b9413357df68cc4dbd1862
平野 宇教 <82029019+TakayukiHirano117@users.noreply.github.com>
2023-02-22 04:08:14 +0900

    Update Cal.java

--- Cal.java
+++ Cal.java
@@ -2,17 +2,24 @@ package chukan_kadai;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Scanner;
 
 // 逆ポーランド記法の数式を計算する処理
 public class Cal {
 	public static double result(String rpnExpression) {
-//		1+( 2 ( 3 + (( 5 / 2 ) - 3 )))
 		
-		String[] formulaArray = rpnExpression.split("");
+		String[] formulaArray = rpnExpression.split(" ");
+		
 		Deque<Double> stack = new ArrayDeque<Double>();
 		
 		for(String e : formulaArray) {
-			if('0' <= e.charAt(0) && e.charAt(0) <= '9') {
+			if(('0' <= e.charAt(0) && e.charAt(0) <= '9'  || 'a' <= e.charAt(0) && e.charAt(0) <= 'z' || 'A' <= e.charAt(0) && e.charAt(0) <= 'Z')) {
+					if(isNum(e) == false) {
+						System.out.println(e + "に数値を入力してください。");
+						Scanner s = new Scanner(System.in);
+						String var = s.nextLine();
+						e = var;
+					}
 //				String型からDoubleにキャスト
 				stack.push(Double.parseDouble(e));
 			} else {
@@ -38,4 +45,14 @@ public class Cal {
 		double result = stack.pop();
 		return result;
 	}
+	
+	public static boolean isNum(String s) {
+	    try {
+	        Integer.parseInt(s);
+	        return true;
+	    } catch (NumberFormatException e) {
+	        return false;
+	    }
+	}
+	
 }
